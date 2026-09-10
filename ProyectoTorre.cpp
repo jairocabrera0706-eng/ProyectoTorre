0e841ea7622365050aae280677313c417e1aaf84
jairocabrera0706 <jairocabrera0706@gmail.com>
2026-09-09 23:10:30 -0400

    Definicion Clase

---
+++ ProyectoTorre.cpp
@@ -0,0 +1,15 @@
+class Torre {
+public:
+    short fila;
+    short columna;
+
+private:
+    bool isBlack;
+
+public:
+    Torre() {
+        fila = 0;
+        columna = 0;
+        isBlack = true;
+    }
+};
